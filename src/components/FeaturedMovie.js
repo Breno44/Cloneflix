e49369a5d17d51fa089e748ad28d81be8daec894
Breno Henrique <bhfs.2908@gmail.com>
2021-08-12 15:25:06 -0300

    feat:responsiveness

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -11,6 +11,11 @@ export default ({item}) => {
         genres.push(item.genres[i].name)
     }
 
+    let description = item.overview;
+    if(description.length > 200) {
+        description = description.substring(0, 200)+'...';
+    };
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -25,7 +30,7 @@ export default ({item}) => {
                         <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's': ''}</div>
                     </div>
-                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--description">{description}</div>
                     <div className="featured--buttons">
                         <a href={`/watch/${item.id}`} className="featured--watchbutton">&#9654; Assistir</a>
                         <a href={`/ist/add/${item.id}`} className="featured--mylistbutton">&#43; Minha Lista</a>
